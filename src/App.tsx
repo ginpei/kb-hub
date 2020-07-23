c1d80ba895f6bc66e4117b4b7422bea611b722b2
ginpei <ginpei@gmail.com>
2020-07-23 02:22:32 -0700

    fix(router): navigating pages always results not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { createBrowserHistory } from "history";
 import React from "react";
-import { Route, Router, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./middleware/firebase";
 import { HomePage } from "./screens/HomePage";
 import { LoginPage } from "./screens/LoginPage";
 import { NotFoundScreen } from "./screens/NotFoundScreen";
 
-const appHistory = createBrowserHistory();
-
 const App: React.FC = () => {
   return (
-    <Router history={appHistory}>
+    <BrowserRouter>
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route exact={true} path="/login" component={LoginPage} />
         <Route component={NotFoundScreen} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 };
 

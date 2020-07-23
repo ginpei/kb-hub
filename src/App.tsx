5319f40552d62a55e5de02c9160cba1e2af1b8bf
ginpei <ginpei@gmail.com>
2020-07-23 00:19:54 -0700

    feat(routing): install router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { createBrowserHistory } from "history";
 import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
 import { HomePage } from "./screens/HomePage";
+import { NotFoundPage } from "./screens/NotFoundPage";
+
+const appHistory = createBrowserHistory();
 
 const App: React.FC = () => {
-  return <HomePage />;
+  return (
+    <Router history={appHistory}>
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;

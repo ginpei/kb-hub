3e09f26634af2f4524b6b2780987edbd9a5be382
ginpei <ginpei@gmail.com>
2020-07-23 19:17:36 -0700

    feat(loading): show smoothly

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "./LoadingScreen.module.scss";
 
 export const LoadingScreen: React.FC = () => {
-  return <div>…</div>;
+  return (
+    <div className="LoadingScreen">
+      <div className={styles.header}></div>
+    </div>
+  );
 };

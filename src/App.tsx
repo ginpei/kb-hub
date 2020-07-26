029965b46f32199ea9a2c95e857b13ec11ccfd09
ginpei <ginpei@gmail.com>
2020-07-26 10:06:29 -0700

    refactor(routings): extract

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import firebase from "firebase/app";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CurrentUserProvider } from "./models/CurrentUserProvider";
-import { GroupEditPage } from "./screens/groups/GroupEditPage";
-import { GroupIndexPage } from "./screens/groups/GroupIndexPage";
-import { GroupViewPage } from "./screens/groups/GroupViewPage";
-import { NewGroupPage } from "./screens/groups/NewGroupPage";
-import { HomePage } from "./screens/HomePage";
-import { KBEditPage } from "./screens/kb/KBEditPage";
-import { KBIndexPage } from "./screens/kb/KBIndexPage";
-import { KBNewPage } from "./screens/kb/KBNewPage";
-import { KBViewPage } from "./screens/kb/KBViewPage";
-import { LoginScreen } from "./screens/LoginScreen";
-import { MyPage } from "./screens/MyPage";
-import { NotFoundScreen } from "./screens/NotFoundScreen";
+import { Routings } from "./Routings";
 
 const auth = firebase.auth();
 const fs = firebase.firestore();
@@ -22,24 +11,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <CurrentUserProvider auth={auth} fs={fs}>
-        <Switch>
-          <Route exact={true} path="/" component={HomePage} />
-          <Route exact={true} path="/groups" component={GroupIndexPage} />
-          <Route exact={true} path="/groups/new" component={NewGroupPage} />
-          <Route exact={true} path="/groups/:id" component={GroupViewPage} />
-          <Route
-            exact={true}
-            path="/groups/:id/edit"
-            component={GroupEditPage}
-          />
-          <Route exact={true} path="/kb" component={KBIndexPage} />
-          <Route exact={true} path="/kb/new" component={KBNewPage} />
-          <Route exact={true} path="/kb/:id" component={KBViewPage} />
-          <Route exact={true} path="/kb/:id/edit" component={KBEditPage} />
-          <Route exact={true} path="/login" component={LoginScreen} />
-          <Route exact={true} path="/my" component={MyPage} />
-          <Route component={NotFoundScreen} />
-        </Switch>
+        <Routings />
       </CurrentUserProvider>
     </BrowserRouter>
   );
---
+++ src/Routings.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { GroupEditPage } from "./screens/groups/GroupEditPage";
+import { GroupIndexPage } from "./screens/groups/GroupIndexPage";
+import { GroupViewPage } from "./screens/groups/GroupViewPage";
+import { NewGroupPage } from "./screens/groups/NewGroupPage";
+import { HomePage } from "./screens/HomePage";
+import { KBEditPage } from "./screens/kb/KBEditPage";
+import { KBIndexPage } from "./screens/kb/KBIndexPage";
+import { KBNewPage } from "./screens/kb/KBNewPage";
+import { KBViewPage } from "./screens/kb/KBViewPage";
+import { LoginScreen } from "./screens/LoginScreen";
+import { MyPage } from "./screens/MyPage";
+import { NotFoundScreen } from "./screens/NotFoundScreen";
+
+export const Routings: React.FC = () => (
+  <Switch>
+    <Route exact={true} path="/" component={HomePage} />
+    <Route exact={true} path="/groups" component={GroupIndexPage} />
+    <Route exact={true} path="/groups/new" component={NewGroupPage} />
+    <Route exact={true} path="/groups/:id" component={GroupViewPage} />
+    <Route exact={true} path="/groups/:id/edit" component={GroupEditPage} />
+    <Route exact={true} path="/kb" component={KBIndexPage} />
+    <Route exact={true} path="/kb/new" component={KBNewPage} />
+    <Route exact={true} path="/kb/:id" component={KBViewPage} />
+    <Route exact={true} path="/kb/:id/edit" component={KBEditPage} />
+    <Route exact={true} path="/login" component={LoginScreen} />
+    <Route exact={true} path="/my" component={MyPage} />
+    <Route component={NotFoundScreen} />
+  </Switch>
+);

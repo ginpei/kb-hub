e1eb6219d545cd72cd0d49e6e411649484d69b87
ginpei <ginpei@gmail.com>
2020-08-02 00:22:58 -0700

    refactor(group user): extract form

--- src/screens/groups/GroupUserManagementPage.tsx.tsx
+++ src/screens/groups/GroupUserManagementPage.tsx.tsx
@@ -2,8 +2,7 @@ import firebase from "firebase/app";
 import React, { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import { BasicLayout } from "../../composites/BasicLayout";
-import { sleep } from "../../misc/misc";
-import { groupPath } from "../../models/Group";
+import { Group, groupPath } from "../../models/Group";
 import {
   createGroupUser,
   privilegeToLabel,
@@ -21,6 +20,41 @@ const fs = firebase.firestore();
 export const GroupUserManagementPage: React.FC = provideGroupPage(() => {
   const group = useGroupPageContext();
   const [users, usersReady, usersError] = useGroupUsers(fs, group);
+
+  if (!usersReady) {
+    return <LoadingScreen />;
+  }
+
+  if (usersError) {
+    return <ErrorScreen error={usersError} />;
+  }
+
+  return (
+    <BasicLayout title={`Manage users - ${group.name}`}>
+      <h1>{group.name} - Manage users</h1>
+      <p>
+        <Link to={groupPath("view", group)}>Back</Link>
+      </p>
+      <NewGroupUserSection group={group} />
+      <ul>
+        {users.map((gUser) => (
+          <li key={gUser.id}>
+            {gUser.user.name}
+            <br />
+            <small style={{ color: "var(--color-moderate-fg)" }}>
+              {"Privileges: "}
+              {gUser.privileges
+                .map((privilege) => privilegeToLabel(privilege))
+                .join(", ")}
+            </small>
+          </li>
+        ))}
+      </ul>
+    </BasicLayout>
+  );
+});
+
+const NewGroupUserSection: React.FC<{ group: Group }> = ({ group }) => {
   const [groupUser, setGroupUser] = useState(createGroupUser({ group }));
   const [savingGroupUser, setSavingGroupUser] = useState(false);
   const [groupUserError, setGroupUserError] = useState<Error | null>(null);
@@ -52,48 +86,20 @@ export const GroupUserManagementPage: React.FC = provideGroupPage(() => {
     }
   }, [groupUser, group]);
 
-  if (!usersReady) {
-    return <LoadingScreen />;
-  }
-
-  if (usersError) {
-    return <ErrorScreen error={usersError} />;
-  }
-
   return (
-    <BasicLayout title={`Manage users - ${group.name}`}>
-      <h1>{group.name} - Manage users</h1>
-      <p>
-        <Link to={groupPath("view", group)}>Back</Link>
-      </p>
-      <details>
-        <summary>Add user</summary>
-        {groupUserError && (
-          <p style={{ color: "tomato" }}>{groupUserError.message}</p>
-        )}
-        <GroupUserForm
-          disabled={savingGroupUser}
-          groupUser={groupUser}
-          onFindUserClick={onFindUserClick}
-          onSubmit={onGroupUserSubmit}
-          onUserIdChange={onUserIdChange}
-          userId={userId}
-        />
-      </details>
-      <ul>
-        {users.map((gUser) => (
-          <li key={gUser.id}>
-            {gUser.user.name}
-            <br />
-            <small style={{ color: "var(--color-moderate-fg)" }}>
-              {"Privileges: "}
-              {gUser.privileges
-                .map((privilege) => privilegeToLabel(privilege))
-                .join(", ")}
-            </small>
-          </li>
-        ))}
-      </ul>
-    </BasicLayout>
+    <details>
+      <summary>Add user</summary>
+      {groupUserError && (
+        <p style={{ color: "tomato" }}>{groupUserError.message}</p>
+      )}
+      <GroupUserForm
+        disabled={savingGroupUser}
+        groupUser={groupUser}
+        onFindUserClick={onFindUserClick}
+        onSubmit={onGroupUserSubmit}
+        onUserIdChange={onUserIdChange}
+        userId={userId}
+      />
+    </details>
   );
-});
+};

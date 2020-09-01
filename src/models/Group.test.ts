6f7700a626fd3d9c301c5294e6eeca6431563dfa
ginpei <ginpei@gmail.com>
2020-09-01 00:50:29 -0700

    test(group user): update rules to use `groups/:id/users`

--- src/models/Group.test.ts
+++ src/models/Group.test.ts
@@ -1,7 +1,8 @@
 import { describeIfEmulatorUp } from "../firestoreTesting";
 import { FirestoreEmu, prepareFirestore } from "../misc/firestore-test";
 import { createGroup, Group } from "./Group";
-import { createGroupUser } from "./GroupUser";
+import { createGroupUser, GroupUser } from "./GroupUser";
+import { createUser } from "./User";
 
 describe("Group", () => {
   describeIfEmulatorUp("rules", () => {
@@ -12,15 +13,26 @@ describe("Group", () => {
         fs = prepareFirestore("user-1");
 
         await createGroupDoc({ id: "group-1", name: "Group 1" });
+        await createGroupUserDoc("group-1", { id: "user-1" });
       });
 
       afterEach(async () => {
         await fs.cleanUp();
       });
 
-      it("can access", async () => {
+      it("can read", async () => {
         const ss = await fs.collection("groups").doc("group-1").get();
         expect(ss.data()?.name).toBe("Group 1");
+      });
+
+      it("can write", async () => {
+        await fs
+          .collection("groups")
+          .doc("group-1")
+          .update({ name: "updated" });
+
+        const ss = await fs.collection("groups").doc("group-1").get();
+        expect(ss.data()?.name).toBe("updated");
       });
     });
 
@@ -29,6 +41,7 @@ describe("Group", () => {
         fs = prepareFirestore(undefined);
 
         await createGroupDoc({ id: "group-1", name: "Group 1" });
+        await createGroupUserDoc("group-1", { id: "user-1" });
       });
 
       afterEach(async () => {
@@ -42,6 +55,31 @@ describe("Group", () => {
       });
     });
 
+    describe("non-participant", () => {
+      beforeAll(async () => {
+        fs = prepareFirestore("user-999");
+
+        await createGroupDoc({ id: "group-1", name: "Group 1" });
+        await createGroupUserDoc("group-1", { id: "user-1" });
+      });
+
+      afterAll(async () => {
+        await fs.cleanUp();
+      });
+
+      it("cannot read", async () => {
+        return expect(
+          fs.collection("groups").doc("group-1").get()
+        ).rejects.toThrow();
+      });
+
+      it("cannot write", async () => {
+        return expect(
+          fs.collection("groups").doc("group-1").set({ name: "updated" })
+        ).rejects.toThrow();
+      });
+    });
+
     async function createGroupDoc(initial: Partial<Group>) {
       if (!initial.id) {
         throw new Error("ID is required for test");
@@ -50,7 +88,28 @@ describe("Group", () => {
       const group = createGroup(initial);
       const doc = fs.admin.collection("groups").doc(group.id);
       await doc.set(group);
-      await doc.collection("users").add(createGroupUser());
+
+      return doc;
+    }
+
+    // TODO fix user data handling
+    async function createGroupUserDoc(
+      groupId: string,
+      initial: Partial<GroupUser>
+    ) {
+      const user = createUser({ id: "user-1" });
+      const groupUser = createGroupUser({
+        id: user.id,
+        privileges: ["login", "userManagement"],
+        user,
+      });
+      const doc = await fs.admin
+        .collection("groups")
+        .doc(groupId)
+        .collection("users")
+        .doc(groupUser.id)
+        .set(groupUser);
+
       return doc;
     }
   });
